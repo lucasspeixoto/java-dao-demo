1bce423431d2bbc57ece3a856621936c1d7bb7db
Lucas Peixoto <lspeixotodev@gmail.com>
2023-02-11 09:40:14 -0300

    Department and Seller entities and daos

---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Department:\n" +
+                "id = " + id +
+                "; name = '" + name + '\'';
+    }
+
+    /**
+     * Se quisermos que nossos objetos sejam convertidos
+     * em sequência de bits, ou seja, quando vamos realizar
+     * gravação em arquivo,trafego em rede, etc...
+     */
+}
